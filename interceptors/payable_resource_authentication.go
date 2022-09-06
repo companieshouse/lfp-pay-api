497d4fd3dfff730fca822c05d14c00fa8d965bfe
adr1ancho <adrian.cho.j@gmail.com>
2022-09-06 12:45:51 +0100

    Address sonar and dependabot issues

--- interceptors/payable_resource_authentication.go
+++ interceptors/payable_resource_authentication.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 
 	"github.com/companieshouse/chs.go/authentication"
-
 	"github.com/companieshouse/chs.go/log"
+	"github.com/companieshouse/lfp-pay-api-core/models"
 	"github.com/companieshouse/lfp-pay-api/config"
 	"github.com/companieshouse/lfp-pay-api/service"
 	"github.com/companieshouse/lfp-pay-api/utils"
@@ -40,7 +40,7 @@ func (payableAuthInterceptor *PayableAuthenticationInterceptor) PayableAuthentic
 
 		// Get identity type from request
 		identityType := authentication.GetAuthorisedIdentityType(r)
-		if !(identityType == authentication.Oauth2IdentityType || identityType == authentication.APIKeyIdentityType) {
+		if isUnauthorizedIdentityType(identityType) {
 			log.InfoR(r, "PayableAuthenticationInterceptor unauthorised: not oauth2 or API key identity type")
 			w.WriteHeader(http.StatusUnauthorized)
 			return
@@ -66,29 +66,9 @@ func (payableAuthInterceptor *PayableAuthenticationInterceptor) PayableAuthentic
 			}
 		}
 
-		// Get the payable resource from the ID in request
-		payableResource, responseType, err := payableAuthInterceptor.Service.GetPayableResource(r, companyNumber, payableID)
-		if err != nil {
-			log.ErrorR(r, fmt.Errorf("PayableAuthenticationInterceptor error when retrieving payable_resource: [%v]", err), log.Data{"service_response_type": responseType.String()})
-			switch responseType {
-			case service.Forbidden:
-				w.WriteHeader(http.StatusForbidden)
-				return
-			default:
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
-
-		if responseType == service.NotFound {
-			log.InfoR(r, "PayableAuthenticationInterceptor not found", log.Data{"payable_id": payableID, "company_number": companyNumber})
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		if responseType != service.Success {
-			log.ErrorR(r, fmt.Errorf("PayableAuthenticationInterceptor error when retrieving payable_resource. Status: [%s]", responseType.String()))
-			w.WriteHeader(http.StatusInternalServerError)
+		payableResource, ret := writeHeader(w, r, payableAuthInterceptor,
+			companyNumber, payableID)
+		if ret {
 			return
 		}
 
@@ -112,31 +92,92 @@ func (payableAuthInterceptor *PayableAuthenticationInterceptor) PayableAuthentic
 			"request_method":                             r.Method,
 		}
 
-		// Now that we have the payable resource data and authorized user there are
-		// multiple cases that can be allowed through:
-		switch {
-		case authUserIsPayableResourceCreator:
-			// 1) Authorized user created the payable_resource
-			log.InfoR(r, "PayableAuthenticationInterceptor authorised as creator", debugMap)
-			// Call the next handler
-			next.ServeHTTP(w, r.WithContext(ctx))
-		case authUserHasPenaltyLookupRole && isGetRequest:
-			// 2) Authorized user has permission to lookup any payable_resource and
-			// request is a GET i.e. to see payable_resource data but not modify/delete
-			log.InfoR(r, "PayableAuthenticationInterceptor authorised as admin penalty lookup role on GET", debugMap)
-			// Call the next handler
-			next.ServeHTTP(w, r.WithContext(ctx))
-		case isAPIKeyRequest && apiKeyHasElevatedPrivileges:
-			// 3) Authorized API key with elevated privileges is an internal API key
-			// that we trust
-			log.InfoR(r, "PayableAuthenticationInterceptor authorised as api key elevated user", debugMap)
-			// Call the next handler
-			next.ServeHTTP(w, r.WithContext(ctx))
-		default:
-			// If none of the above conditions above are met then the request is
-			// unauthorized
-			w.WriteHeader(http.StatusUnauthorized)
-			log.InfoR(r, "PayableAuthenticationInterceptor unauthorised", debugMap)
-		}
+		checkAllowedThrough(w, r, authUserIsPayableResourceCreator, debugMap, next, ctx, authUserHasPenaltyLookupRole, isGetRequest, isAPIKeyRequest, apiKeyHasElevatedPrivileges)
 	})
+}
+
+func checkAllowedThrough(w http.ResponseWriter,
+	r *http.Request,
+	authUserIsPayableResourceCreator bool,
+	debugMap log.Data,
+	next http.Handler,
+	ctx context.Context,
+	authUserHasPenaltyLookupRole bool,
+	isGetRequest bool,
+	isAPIKeyRequest bool,
+	apiKeyHasElevatedPrivileges bool) {
+	// Now that we have the payable resource data and authorized user there are
+	// multiple cases that can be allowed through:
+	switch {
+	case authUserIsPayableResourceCreator:
+		// 1) Authorized user created the payable_resource
+		log.InfoR(r, "PayableAuthenticationInterceptor authorised as creator", debugMap)
+		// Call the next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	case isAuthorizedGetRequest(authUserHasPenaltyLookupRole, isGetRequest):
+		// 2) Authorized user has permission to lookup any payable_resource and
+		// request is a GET i.e. to see payable_resource data but not modify/delete
+		log.InfoR(r, "PayableAuthenticationInterceptor authorised as admin penalty lookup role on GET", debugMap)
+		// Call the next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	case isAuthorizedApiKeyRequest(isAPIKeyRequest, apiKeyHasElevatedPrivileges):
+		// 3) Authorized API key with elevated privileges is an internal API key
+		// that we trust
+		log.InfoR(r, "PayableAuthenticationInterceptor authorised as api key elevated user", debugMap)
+		// Call the next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	default:
+		// If none of the above conditions above are met then the request is
+		// unauthorized
+		w.WriteHeader(http.StatusUnauthorized)
+		log.InfoR(r, "PayableAuthenticationInterceptor unauthorised", debugMap)
+	}
+}
+
+func writeHeader(w http.ResponseWriter,
+	r *http.Request,
+	payableAuthInterceptor *PayableAuthenticationInterceptor,
+	companyNumber string,
+	payableID string) (*models.PayableResource, bool) {
+	// Get the payable resource from the ID in request
+	payableResource, responseType, err := payableAuthInterceptor.Service.GetPayableResource(r, companyNumber, payableID)
+	if err != nil {
+		log.ErrorR(r, fmt.Errorf("PayableAuthenticationInterceptor error when retrieving payable_resource: [%v]", err), log.Data{"service_response_type": responseType.String()})
+		switch responseType {
+		case service.Forbidden:
+			w.WriteHeader(http.StatusForbidden)
+			return nil, true
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			return nil, true
+		}
+	}
+
+	if responseType == service.NotFound {
+		log.InfoR(r, "PayableAuthenticationInterceptor not found", log.Data{"payable_id": payableID, "company_number": companyNumber})
+		w.WriteHeader(http.StatusNotFound)
+		return nil, true
+	}
+
+	if responseType != service.Success {
+		log.ErrorR(r, fmt.Errorf("PayableAuthenticationInterceptor error when retrieving payable_resource. Status: [%s]", responseType.String()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return nil, true
+	}
+	return payableResource, false
+}
+
+func isUnauthorizedIdentityType(identityType string) bool {
+	return !(identityType == authentication.Oauth2IdentityType ||
+		identityType == authentication.APIKeyIdentityType)
+}
+
+func isAuthorizedApiKeyRequest(isAPIKeyRequest bool,
+	apiKeyHasElevatedPrivileges bool) bool {
+	return isAPIKeyRequest && apiKeyHasElevatedPrivileges
+}
+
+func isAuthorizedGetRequest(authUserHasPenaltyLookupRole bool,
+	isGetRequest bool) bool {
+	return authUserHasPenaltyLookupRole && isGetRequest
 }
